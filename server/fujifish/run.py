48cd754b2d440c573973a8dc734cd7389aeb9643
radioation <greggallardo@gmail.com>
2026-09-04 00:13:56 -0500

    getting server to run without lobby for now

--- server/fujifish/run.py
+++ server/fujifish/run.py
@@ -4,10 +4,24 @@ from dotenv import load_dotenv
 
 import threading, socketserver
 
-from fujifish.http_api import app, initialize_tables
-from lobby.lobby_client import init_lobby
+from api.http_api import app #, initialize_tables
+from api.tcp_api import TcpChessHandler
+
+# don't lobby yet.
+# from lobby.lobby_client import init_lobby
 
 
+tcp_server = None
+
+def start_tcp():
+    global tcp_server
+    
+
+def shutdown( signum, frame ):
+    if tcp_server:
+        tcp_server.shutdown()
+        tcp_server.server_close()
+    sys.exit(0)
 
 if __name__ == "__main__":
 
@@ -16,11 +30,11 @@ if __name__ == "__main__":
 
     server_host = os.getenv('SERVER_HOST', "0.0.0.0")
     server_port = int(os.getenv('SERVER_PORT', 8080))
-    lobby_endpoint = os.getenv('LOBBY_ENDPOINT_UPSERT')
-    init_lobby( lobby_endpoint )
-    
 
-    initialize_tables()
+
+    #lobby_endpoint = os.getenv('LOBBY_ENDPOINT_UPSERT')
+    #init_lobby( lobby_endpoint )
+    #initialize_tables()
 
     app.run(host=server_host, port=server_port)
 
